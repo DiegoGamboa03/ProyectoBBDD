49118c2b9c53f44cc032ecca359395be01eb32e3
Diego <diego.gamboaa03@gmail.com>
2021-12-23 21:25:50 -0400

    Agregando archivos C#

---
+++ ProyectoBBDD/ProyectoBBDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoBBDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
