d649760bce6d0f3ebc2daed0d58f1f53c8a081dd
Diego <diego.gamboaa03@gmail.com>
2022-01-25 14:20:44 -0400

    Registro de estudiante exitoso

--- Proyecto base de datos/Proyecto base de datos/pages/RegisterStudent.xaml.cs
+++ Proyecto base de datos/Proyecto base de datos/pages/RegisterStudent.xaml.cs
@@ -28,18 +28,19 @@ namespace Proyecto_base_de_datos.pages
         {
             var conn = new Connection();
             conn.openConnection();
-            using (var command = new NpgsqlCommand("INSERT INTO estudiantes (cedulae, nombre, telefono, correoucab, correoparticular, contrasena, otro) VALUES (@n1, @q1, @n2, @q2, @n3, @q3)", conn.conn))
-             {
-                 command.Parameters.AddWithValue("n1", "banana");
-                 command.Parameters.AddWithValue("q1", "150");
-                 command.Parameters.AddWithValue("n2", "orange");
-                 command.Parameters.AddWithValue("q2", "154");
-                 command.Parameters.AddWithValue("n3", "apple");
-                 command.Parameters.AddWithValue("q3", "100");
+            using (var command = new NpgsqlCommand("INSERT INTO estudiantes (cedulae, nombre, telefono, correoucab, correoparticular, contrasena, otro) VALUES (@n1, @q1, @n2, @q2, @n3, @q3, @n4)", conn.conn))
+            {
+                command.Parameters.AddWithValue("n1", idTextBox.Text);
+                command.Parameters.AddWithValue("q1", nameTextBox.Text);
+                command.Parameters.AddWithValue("n2", phoneNumberTextBox.Text);
+                command.Parameters.AddWithValue("q2", ucabMailTextBox.Text);
+                command.Parameters.AddWithValue("n3", mailTextBox.Text);
+                command.Parameters.AddWithValue("q3", passwordTextBox.Text);
+                command.Parameters.AddWithValue("n4", bonusAtributteTextBox.Text);
 
-                 int nRows = command.ExecuteNonQuery();
-                 Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
-             }
+                int nRows = command.ExecuteNonQuery();
+                Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
+            }
         }
     }
 }
