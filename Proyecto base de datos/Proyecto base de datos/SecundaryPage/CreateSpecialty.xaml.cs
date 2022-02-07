ccec145961cb83f2eb8102097a980296bfb17ed9
Diego <diego.gamboaa03@gmail.com>
2022-02-07 04:08:51 -0400

    Creacion de especialidades

---
+++ Proyecto base de datos/Proyecto base de datos/SecundaryPage/CreateSpecialty.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Npgsql;
+using Proyecto_base_de_datos.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_base_de_datos.SecundaryPage
+{
+    /// <summary>
+    /// Interaction logic for CreateSpecialty.xaml
+    /// </summary>
+    public partial class CreateSpecialty : Page
+    {
+        public CreateSpecialty()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            var conn = new Connection();
+            conn.openConnection();
+            using (var command = new NpgsqlCommand("INSERT INTO especialidades (codesp,nombreesp) VALUES (nextval('especialidadesPKsecuencia'), @n1)", conn.conn))
+            {
+                command.Parameters.AddWithValue("n1",specialtyNameTextBox.Text.Trim());
+                int nRows = command.ExecuteNonQuery();
+            }
+        }
+    }
+}
