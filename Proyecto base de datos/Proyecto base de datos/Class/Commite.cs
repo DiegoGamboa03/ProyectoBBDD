3b880188be8f5f4912cf9f78827bdcdc463ce921
Diego <diego.gamboaa03@gmail.com>
2022-02-10 16:18:49 -0400

    Creando clases de comite y consejo

---
+++ Proyecto base de datos/Proyecto base de datos/Class/Commite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_base_de_datos.Class
+{
+    class Commite
+    {
+        String commiteID;
+        DateTime commiteDate;
+
+        public Commite(string commiteID, DateTime commiteDate)
+        {
+            this.commiteID = commiteID;
+            this.commiteDate = commiteDate;
+        }
+
+        public string CommiteID { get => commiteID; set => commiteID = value; }
+        public DateTime CommiteDate { get => commiteDate; set => commiteDate = value; }
+    }
+}
---
+++ Proyecto base de datos/Proyecto base de datos/Class/Council.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_base_de_datos.Class
+{
+    class Council
+    {
+        String councilID;
+        DateTime councilDate;
+
+        public Council(string councilID, DateTime councilDate)
+        {
+            this.councilID = councilID;
+            this.councilDate = councilDate;
+        }
+
+        public string CouncilID { get => councilID; set => councilID = value; }
+        public DateTime CouncilDate { get => councilDate; set => councilDate = value; }
+    }
+}
