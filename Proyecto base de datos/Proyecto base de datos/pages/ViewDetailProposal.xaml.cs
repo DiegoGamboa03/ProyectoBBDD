7d1a21f74b7b631d4d4833e71508fa1c073a23de
Roberson Garcia <roberson.gar12@gmail.com>
2022-02-15 00:26:12 -0400

    Se agregaron cosas al detalle de las propuesta

--- Proyecto base de datos/Proyecto base de datos/pages/ViewDetailProposal.xaml.cs
+++ Proyecto base de datos/Proyecto base de datos/pages/ViewDetailProposal.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Proyecto_base_de_datos.Class;
+using Npgsql;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -28,23 +29,73 @@ namespace Proyecto_base_de_datos.pages
                 modality.Text = "Instrumental";
             else
                 modality.Text = "Experimental";
-            observations.Text = degreeWorks.Observations;
-            ncouncil.Text = degreeWorks.CouncilNumber;
-            ncomitte.Text = degreeWorks.IdCouncil;
-            student.Visibility = Visibility.Collapsed;
-            students.Visibility = Visibility.Collapsed;
+            if (degreeWorks.Observations == "")
+                observations.Text = "No tiene observaciones";
+            else
+                observations.Text = degreeWorks.Observations;
+
+            if (degreeWorks.CouncilNumber == "")
+                ncouncil.Text = "No tiene consejo asignado";
+            else
+                ncouncil.Text = degreeWorks.CouncilNumber;
+
+            if (degreeWorks.IdCouncil == "")
+                ncomitte.Text = "No tiene comité asignado";
+            else
+                ncomitte.Text = degreeWorks.IdCouncil;            
 
             if (degreeWorks.IdInternTeacher == "")
                 teacher.Text = "No asignado";
+            else
+                SearchTeacher(degreeWorks.IdInternTeacher);
+            
+            student.Visibility = Visibility.Collapsed;
+            students.Visibility = Visibility.Collapsed;
+
+            SearchStudents(degreeWorks.CorrelativeNumber);
         }
-        private void SearchTeacher()
+        private void SearchTeacher(string idTeacher)
         {
-
+            var conn = new Connection();
+            conn.openConnection();
+            using (var command = new NpgsqlCommand("SELECT * FROM \"profesores\" WHERE \"cedulap\" = '" + idTeacher  + "'", conn.conn))
+            {
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                   String name = (String)reader["nombre"];
+                    teacher.Text = name;
+                }
+                reader.Close();
+            }
         }
 
-        private void SearchStudents()
+        private void SearchStudents(int nro)
         {
+            List<String> studentsNames = new List<String>();
+            var conn = new Connection();
+            conn.openConnection();
+            using (var command = new NpgsqlCommand("SELECT * FROM\"entrega\" \"e\",\"estudiantes\" \"es\" WHERE e.\"ncorrelativo\" = '" + nro.ToString()+"' AND e.cedulae = es.cedulae", conn.conn))
+            {
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    studentsNames.Add(((String)reader["nombre"]));     
+                }
+                reader.Close();
 
+                if (studentsNames.Count == 1)
+                {
+                    student.Visibility = Visibility.Visible;
+                    studentName.Text = studentsNames[0];
+                } else if (studentsNames.Count == 2)
+                {
+                    students.Visibility = Visibility.Visible;
+                    studentName1.Text = studentsNames[0];
+                    studentName2.Text = studentsNames[1];
+                }
+
+            }
         }
     }
 }
